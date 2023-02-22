38d6f6b2844b1ed84fbe4278d1e4013eee0c3131
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-22 11:20:33 +0300

    Фремфорк Jest: установка пакета, проведен первый тест

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hexlet_JavaScript_autotesting
 Мои решения задач для курса "JS: Автоматическое тестирование"
+
+## Вызов тестов JEST для вызова в случае использования ECMAScript модулей ("type": "module"):
+- *NODE_OPTIONS=--experimental-vm-modules npx jest*
+- *node --experimental-vm-modules node_modules/jest/bin/jest.js*
--- tests/functions.test.js
+++ tests/functions.test.js
@@ -1,16 +1,12 @@
-import { strict as assert } from "assert";
-import getFunction from "../src/functions.js";
+import reverse from "../src/functions.js";
 
-const indexOf = getFunction();
+test("reverse", () => {
+  const str = "hello";
+  expect(reverse(str)).toEqual("olleh");
+  expect(reverse("")).toEqual("");
+});
 
-assert.deepEqual(indexOf([1, 2, 1, 2], 2), 1);
-assert.deepEqual(indexOf([1, 2, 1, 2], 2, 2), 3);
-assert.deepEqual(indexOf([1, 2, 1, 2], 10), -1);
-assert.deepEqual(indexOf([0], 0), 0);
 
-assert(indexOf([1, 2, 1, 2], 2) === 1);
-assert(indexOf([1, 2, 1, 2], 2, 2) === 3);
-assert(indexOf([1, 2, 1, 2], 10) === -1);
-assert(indexOf([0], 0) === 0);
-
-console.log("Done");
+// "scripts": {
+//   "test": "echo \"Error: no test specified\" && exit 1"
+// },
