ec1363547e83fafc9fbf87cc6db920be8dfa9eaf
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-25 12:09:20 +0300

    Функции makeSegment(), getMidpointOfSegment(), getBeginPoint(), getEndPoint()

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-// Функция calculateDistance, которая находит расстояние между двумя точками на плоскости
-
-const getMidpoint = (point1, point2) => {
-  const x = (point1.x + point2.x) / 2;
-  const y = (point1.y + point2.y) / 2;
-
-  return {x, y};
+const makeDecartPoint = (x, y) => {
+  const point = { x, y };
+  return point;
 };
 
-const point1 = { x: -1, y: 10 };
-const point2 = { x: 0, y: -3 };
-const point3 = getMidpoint(point1, point2);
+const getX = (point) => point.x;
 
-console.log(point3);
+const getY = (point) => point.y;
+
+// Функция makeSegment(). Принимает на вход две точки и возвращает отрезок.
+// Функция getMidpointOfSegment(). Принимает на вход отрезок и возвращает точку находящуюся на середине отрезка.
+// Функция getBeginPoint(). Принимает на вход отрезок и возвращает точку начала отрезка.
+// Функция getEndPoint(). Принимает на вход отрезок и возвращает точку конца отрезка.
+
+const makeSegment = (begin, end) => ({ begin, end });
+
+const getMidpointOfSegment = (segment) => {
+  const { begin, end } = segment;
+  const x = (getX(begin) + getX(end)) / 2;
+  const y = (getY(begin) + getY(end)) / 2;
+
+  return makeDecartPoint(x, y);
+};
+
+const getBeginPoint = (segment) => segment.begin;
+
+const getEndPoint = (segment) => segment.end;
+
+const beginPoint = makeDecartPoint(3, 2);
+const endPoint = makeDecartPoint(0, 0);
+
+const segment = makeSegment(makeDecartPoint(3, 2), makeDecartPoint(0, 0));
+
+console.log(segment);
+console.log(getMidpointOfSegment(segment));
+console.log(getBeginPoint(segment));
+console.log(getEndPoint(segment));
